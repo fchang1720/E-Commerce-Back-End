8f2f63e7693083a04e726bd7b01ec676e027c956
Crucix <fchang1720@gmail.com>
2022-04-28 18:18:53 -0500

    category routes work

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -39,7 +39,8 @@ router.post('/', async (req, res) => {
   // create a new category
   try {
     const categoryData = await Category.create({
-      category_id: req.body.category_id,
+ 
+      category_name: req.body.category_name
     });
     res.status(200).json(categoryData);
   } catch (err) {
