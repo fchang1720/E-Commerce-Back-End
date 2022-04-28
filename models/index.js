28cbb8ee66408559a1038a2ea9d12c327d5b1a26
Crucix <fchang1720@gmail.com>
2022-04-27 20:13:54 -0500

    modified product relationship

--- models/index.js
+++ models/index.js
@@ -16,6 +16,15 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,
+  {
+      through: {
+          model: ProductTag,
+          unique:false
+      },
+      as: 'product_tags'
+  }
+);
 
 // Tags belongToMany Products (through ProductTag)
 
