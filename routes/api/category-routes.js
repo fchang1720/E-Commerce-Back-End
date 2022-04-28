afb7f0fc15a1c9607add7fc5175e9265182f7988
Crucix <fchang1720@gmail.com>
2022-04-27 20:31:21 -0500

    added find by id route for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category id found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
