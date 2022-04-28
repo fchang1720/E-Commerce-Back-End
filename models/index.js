043d9631677625a47be77dc1940c302c550f10ed
Crucix <fchang1720@gmail.com>
2022-04-27 20:15:21 -0500

    finished modifying models index.js

--- models/index.js
+++ models/index.js
@@ -27,6 +27,15 @@ Product.belongsToMany(Tag,
 );
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,
+  {
+      through: {
+          model: ProductTag,
+          unique:false
+      },
+      as: 'tag_products'
+  }
+);
 
 module.exports = {
   Product,
